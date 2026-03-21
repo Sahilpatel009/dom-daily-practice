f9fcea79ca390909bd4ed4d7a2e84d0aaad4a7ec
Sahil_Patel <sahilbpatel009@gmail.com>
2026-03-21 10:37:27 +0530

    create element and appending a child in html

---
+++ class-44/script.js
@@ -0,0 +1,22 @@
+// let btn = document.querySelector("button")
+// let h1 = document.createElement("h1")
+//     h1.innerHTML = 'hello h1'
+// console.log(h1);
+// let main = document.querySelector('main')
+// main.appendChild(h1)
+// btn.addEventListener("click", function () {
+//     let h1 = document.createElement("h1")
+//     h1.innerHTML = 'hello h1'
+//     console.log(h1);
+//     let main = document.querySelector('main')
+//     main.appendChild(h1)
+
+// })
+
+
+let h1 = document.createElement('h1')
+h1.innerHTML = 'create h1'
+h1.style.color = 'red'
+h1.style.backgroundColor = 'goldenrod'
+let main = document.querySelector('main')
+main.appendChild(h1)
