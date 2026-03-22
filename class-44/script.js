b12947c2bce3c83d19f3040f93135df197bd115d
Sahil_Patel <sahilbpatel009@gmail.com>
2026-03-22 20:34:21 +0530

    creating random boxes step 1 in progress

--- class-44/script.js
+++ class-44/script.js
@@ -18,3 +18,21 @@ btn.addEventListener('click', function () {
     div.style.left = x + '%'
     div.style.top = y + '%'
     div.style.transition = '0.5s ease'
+
+    let baseTransform = ''
+
+    let shape = Math.floor(Math.random() * 8)
+
+    if (shape === 0) div.style.borderRadius = '50%'
+    else if (shape === 1) div.style.borderRadius = '0%'
+    else if (shape === 2) div.style.borderRadius = '15px'
+    else if (shape === 3) div.style.clipPath = 'polygon(50% 0%, 0% 100%, 100% 100%)'
+    else if (shape === 4) div.style.clipPath = 'polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)'
+    else if (shape === 5) div.style.clipPath = 'polygon(50% 0%, 100% 38%, 82% 100%, 18% 100%, 0% 38%)'
+    else if (shape === 6) {
+        baseTransform = 'rotate(45deg)'
+        div.style.transform = baseTransform
+    }
+    else if (shape === 7) div.style.clipPath = 'polygon(50% 0%, 60% 35%, 100% 35%, 65% 55%, 80% 100%, 50% 70%, 20% 100%, 35% 55%, 0% 35%, 40% 35%)'
+
+   
