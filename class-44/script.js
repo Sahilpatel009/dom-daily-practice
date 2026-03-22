44313e834f78d8a615fe042c3576b80914099d7a
Sahil_Patel <sahilbpatel009@gmail.com>
2026-03-22 20:12:00 +0530

    creating random boxes

--- class-44/script.js
+++ class-44/script.js
@@ -1,22 +1,65 @@
-// let btn = document.querySelector("button")
-// let h1 = document.createElement("h1")
-//     h1.innerHTML = 'hello h1'
-// console.log(h1);
-// let main = document.querySelector('main')
-// main.appendChild(h1)
-// btn.addEventListener("click", function () {
-//     let h1 = document.createElement("h1")
-//     h1.innerHTML = 'hello h1'
-//     console.log(h1);
-//     let main = document.querySelector('main')
-//     main.appendChild(h1)
-
-// })
-
-
-let h1 = document.createElement('h1')
-h1.innerHTML = 'create h1'
-h1.style.color = 'red'
-h1.style.backgroundColor = 'goldenrod'
+let btn = document.querySelector('button')
 let main = document.querySelector('main')
-main.appendChild(h1)
+
+btn.addEventListener('click', function () {
+    let div = document.createElement('div')
+
+    let x = Math.random() * 100
+    let y = Math.random() * 100
+
+    let c1 = Math.floor(Math.random() * 256)
+    let c2 = Math.floor(Math.random() * 256)
+    let c3 = Math.floor(Math.random() * 256)
+
+    div.style.width = '60px'
+    div.style.height = '60px'
+    div.style.backgroundColor = `rgb(${c1},${c2},${c3})`
+    div.style.position = 'absolute'
+    div.style.left = x + '%'
+    div.style.top = y + '%'
+    div.style.transition = '0.5s ease'
+
+    let baseTransform = ''
+
+    let shape = Math.floor(Math.random() * 8)
+
+    if (shape === 0) div.style.borderRadius = '50%'
+    else if (shape === 1) div.style.borderRadius = '0%'
+    else if (shape === 2) div.style.borderRadius = '15px'
+    else if (shape === 3) div.style.clipPath = 'polygon(50% 0%, 0% 100%, 100% 100%)'
+    else if (shape === 4) div.style.clipPath = 'polygon(25% 0%, 75% 0%, 100% 50%, 75% 100%, 25% 100%, 0% 50%)'
+    else if (shape === 5) div.style.clipPath = 'polygon(50% 0%, 100% 38%, 82% 100%, 18% 100%, 0% 38%)'
+    else if (shape === 6) {
+        baseTransform = 'rotate(45deg)'
+        div.style.transform = baseTransform
+    }
+    else if (shape === 7) div.style.clipPath = 'polygon(50% 0%, 60% 35%, 100% 35%, 65% 55%, 80% 100%, 50% 70%, 20% 100%, 35% 55%, 0% 35%, 40% 35%)'
+
+    div.animate([
+        { transform: baseTransform + ' scale(0)' },
+        { transform: baseTransform + ' scale(1)' }
+    ], { duration: 400, fill: 'forwards' })
+
+    
+    div.addEventListener('click', function () {
+        div.style.transform = baseTransform + ' scale(2)'
+        div.style.opacity = '0'
+
+        clearInterval(moveInterval)
+
+        setTimeout(() => {
+            div.remove()
+        }, 300)
+    })
+
+
+    div.addEventListener('mouseenter', function () {
+        div.style.transform = baseTransform + ' scale(1.5)'
+    })
+
+    div.addEventListener('mouseleave', function () {
+        div.style.transform = baseTransform
+    })
+
+    main.appendChild(div)
+})
