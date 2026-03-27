f19e4b83e0c7b218bebf31b330ab30a1b0b88c36
Sahil_Patel <sahilbpatel009@gmail.com>
2026-03-27 12:16:39 +0530

    Creating a setInterval function

--- class-45/script.js
+++ class-45/script.js
@@ -1,9 +1,3 @@
-let h1 = document.querySelector('h1')
-let btn = document.querySelector('button')
-btn.addEventListener('click', function () {
-    h1.innerHTML = "Changing User..."
-    setTimeout(function () {
-        h1.innerHTML = "Hello I am Karan"
-    }, 2000)
-})
-
+setInterval(() => {
+    console.log('hello')
+}, 1000)
