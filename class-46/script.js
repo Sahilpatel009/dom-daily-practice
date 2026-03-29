e07db2aecfb4b0b50aa3e6f0f445260d61800fa5
Sahil_Patel <sahilbpatel009@gmail.com>
2026-03-29 18:41:12 +0530

    create a card and also creating a instragram like feature

--- class-46/script.js
+++ class-46/script.js
@@ -1,6 +1,12 @@
 let img = document.querySelector('img')
 let love = document.querySelector('i')
-img.addEventListener('dblclick', function () {
-    love.style.opacity = 0.9
-    transform = 'translate(-50%, -50%) scale (1) rotate (0deg)'
+
+img.addEventListener('click', function () {
+    love.style.opacity = 1
+    love.style.transform = 'translate(-50%, -50%) scale(1) rotate(0deg)'
+
+    setTimeout(() => {
+        love.style.opacity = 0
+        love.style.transform = 'translate(-50%, -50%) scale(0) rotate(-60deg)'
+    }, 800)
 })
