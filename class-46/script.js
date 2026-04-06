4a7bba908de68788e02a53772808b8c7c9c8d452
Sahil_Patel <sahilbpatel009@gmail.com>
2026-04-06 10:33:25 +0530

    create a card and and add remove friend button

--- class-46/script.js
+++ class-46/script.js
@@ -2,11 +2,13 @@ let img = document.querySelector('img')
 let love = document.querySelector('i')
 
 img.addEventListener('click', function () {
+
     love.style.opacity = 1
     love.style.transform = 'translate(-50%, -50%) scale(1) rotate(0deg)'
 
     setTimeout(() => {
-        love.style.opacity = 0
-        love.style.transform = 'translate(-50%, -50%) scale(0) rotate(-60deg)'
+
+        love.style.transform = 'translate(-50%, -300%) scale(1) rotate(60deg)'
     }, 800)
+ 
 })
