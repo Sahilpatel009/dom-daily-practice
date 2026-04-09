c99645d153d294a8f7b4ca2f05a43c48ea31b106
Sahil_Patel <sahilbpatel009@gmail.com>
2026-04-09 10:29:29 +0530

    creating a another insta like feature all done

--- project-2-Insta-like-feature/script.js
+++ project-2-Insta-like-feature/script.js
@@ -2,14 +2,14 @@ let box = document.querySelector('.box');
 let heart = document.querySelector('i');
 
 box.addEventListener('dblclick', function () {
- 
+
     heart.style.opacity = 0.9;
     heart.style.transform = "translate(-50%, -50%) scale(1.2)";
-
+    heart.style.transition= "all 0.5s cubic - bezier(0.075, 0.82, 0.165, 1)"
 
     setTimeout(() => {
         heart.style.opacity = 0;
-        
+
         heart.style.transform = "translate(-50%, -150%) scale(0)";
-    }, 800); 
+    }, 800);
 });
